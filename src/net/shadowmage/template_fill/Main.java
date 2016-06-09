6b2c77cf7a98be250865f4e1d8974b3e3f3834ab
shadowmage45 <shadowmage45@hotmail.com>
2016-06-09 10:41:57 -0600

    cleanup libs/dependencies to only those mandatory to function compile working .jar from decomp code + recent fixes; still more to be done

--- src/net/shadowmage/template_fill/Main.java
+++ src/net/shadowmage/template_fill/Main.java
@@ -13,61 +13,68 @@ import org.jopendocument.dom.template.TemplateException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-/*
- * This class specifies class file version 49.0 but uses Java 6 signatures.  Assumed Java 6.
- */
-public class Main {
-    public static void main(String[] aArgs) throws IOException, TemplateException, JDOMException {
-        File templateFile = new File(aArgs[0]);
-        File output = new File(aArgs[1]);
-        File inputFile = new File(aArgs[2]);
-        String inputText = new String(Files.readAllBytes(inputFile.toPath()));
-        JavaScriptFileTemplate template = new JavaScriptFileTemplate(templateFile);
-        JSONObject jo = new JSONObject(inputText);
-        JSONObject dataFields = jo.getJSONObject("dataFields");
-        for (String dataKey : dataFields.keySet()) {
-            String value = dataFields.getString(dataKey);
-            template.setField(dataKey, (Object)value);
-        }
-        JSONObject tablesObject = jo.getJSONObject("tableData");
-        if (tablesObject != null) {
-            for (String tableName : tablesObject.keySet()) {
-                template.setField(tableName, Main.createTable(tablesObject.getJSONObject(tableName)));
-            }
-        }
-        template.createDocument().saveToPackageAs(output);
-        try {
-            String outputFileName = output.getAbsolutePath();
-            int end = outputFileName.lastIndexOf(46);
-            outputFileName = outputFileName.substring(0, end);
-            PDFConverter.saveAsPdf(output.getAbsolutePath(), String.valueOf(outputFileName) + ".pdf");
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        Files.delete(inputFile.toPath());
+public class Main
+{
+  public static void main(String[] aArgs) throws IOException, TemplateException, JDOMException
+  {
+    File templateFile = new File(aArgs[0]);
+    File output = new File(aArgs[1]);
+    File inputFile = new File(aArgs[2]);
+    String inputText = new String(Files.readAllBytes(inputFile.toPath()));
+    JavaScriptFileTemplate template = new JavaScriptFileTemplate(templateFile);
+    JSONObject jo = new JSONObject(inputText);
+    JSONObject dataFields = jo.getJSONObject("dataFields");
+    for (String dataKey : dataFields.keySet())
+    {
+      String value = dataFields.getString(dataKey);
+      template.setField(dataKey, (Object) value);
     }
-
-    private static List<HashMap<String, String>> createTable(JSONObject tableData) {
-        ArrayList<HashMap<String, String>> outputList = new ArrayList<HashMap<String, String>>();
-        JSONArray columnNames = tableData.getJSONArray("columnNames");
-        JSONArray entriesObject = tableData.getJSONArray("entries");
-        int len = entriesObject.length();
-        int colLen = columnNames.length();
-        int i = 0;
-        while (i < len) {
-            HashMap<String, String> entryMap = new HashMap<String, String>();
-            JSONObject entryObject = entriesObject.getJSONObject(i);
-            int k = 0;
-            while (k < colLen) {
-                String colName = columnNames.getString(k);
-                entryMap.put(colName, entryObject.getString(colName));
-                ++k;
-            }
-            outputList.add(entryMap);
-            ++i;
-        }
-        return outputList;
+    JSONObject tablesObject = jo.getJSONObject("tableData");
+    if (tablesObject != null)
+    {
+      for (String tableName : tablesObject.keySet())
+      {
+        template.setField(tableName, Main.createTable(tablesObject.getJSONObject(tableName)));
+      }
     }
+    template.createDocument().saveToPackageAs(output);
+    try
+    {
+      String outputFileName = output.getAbsolutePath();
+      int end = outputFileName.lastIndexOf(46);
+      outputFileName = outputFileName.substring(0, end);
+      PDFConverter.saveAsPdf(output.getAbsolutePath(),
+          String.valueOf(outputFileName) + ".pdf");
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+    Files.delete(inputFile.toPath());
+  }
+  
+  private static List<HashMap<String, String>> createTable(JSONObject tableData)
+  {
+    ArrayList<HashMap<String, String>> outputList = new ArrayList<HashMap<String, String>>();
+    JSONArray columnNames = tableData.getJSONArray("columnNames");
+    JSONArray entriesObject = tableData.getJSONArray("entries");
+    int len = entriesObject.length();
+    int colLen = columnNames.length();
+    int i = 0;
+    while (i < len)
+    {
+      HashMap<String, String> entryMap = new HashMap<String, String>();
+      JSONObject entryObject = entriesObject.getJSONObject(i);
+      int k = 0;
+      while (k < colLen)
+      {
+        String colName = columnNames.getString(k);
+        entryMap.put(colName, entryObject.getString(colName));
+        ++k;
+      }
+      outputList.add(entryMap);
+      ++i;
+    }
+    return outputList;
+  }
 }
-
